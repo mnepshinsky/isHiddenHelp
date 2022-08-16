071cbc4d28c9830492a9321563ab3a1d33b7d730
Megan <meganmarieesl@gmail.com>
2022-08-16 07:49:38 -1000

    I am making a change to my storyboard by adding an additional button!

--- isHiddenHelp/ViewController.swift
+++ isHiddenHelp/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var labelCheck: UILabel!
+    
+    @IBOutlet weak var textField1: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //make sure to put your isHidden code within the viewDidLoad function :D
+        textField1.isHidden = true;
     }
+    
 
-
+    
+    @IBAction func submitButton(_ sender: Any) {
+        textField1.isHidden = false; 
+    }
+    
 }
 
